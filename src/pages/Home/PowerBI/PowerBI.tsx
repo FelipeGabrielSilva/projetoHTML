6eae6c30aca093c7627f7bf3b6bdbbb4560160e9
FelipeGabrielSilva <felipegabriel1106@outlook.com>
2023-11-02 17:45:33 -0300

    Adicionando conteúdo #PowerBI

--- src/pages/Home/PowerBI/PowerBI.tsx
+++ src/pages/Home/PowerBI/PowerBI.tsx
@@ -1,8 +1,61 @@
+import { Flex, Box, Text } from "@chakra-ui/react";
 
 export default function PowerBI() {
-    return(
-        <>
-        
-        </>
-    )
+  return (
+    <Flex w="100%" h="auto" bg="#427D9D">
+      <Box display="flex" flexDir="column" w="60%" p="5em" gap="1em">
+        <img src="https://blog.unipar.br/wp-content/uploads/2021/11/post_thumbnail-801c53f03508c73882d8a372d4df2e17.jpeg" />
+        <Text fontSize="sm">Descrição imagem</Text>
+        <Text fontSize="xl" fontWeight="bold">
+          Visualização de Dados com Power BI
+        </Text>
+        <Text>
+          Uma introdução ao Power BI, uma ferramenta de visualização de dados
+          poderosa da Microsoft, e sua capacidade de transformar dados brutos em
+          informações significativas. Instruções detalhadas sobre como conectar
+          fontes de dados ao Power BI e criar relatórios interativos. Exemplos
+          de relatórios e dashboards criados com o Power BI, incluindo a criação
+          de um dashboard de vendas com filtros interativos e gráficos
+          dinâmicos. Página 5: Introdução ao SQL para Gerenciamento de Dados
+        </Text>
+        <Text>
+          Uma introdução ao Power BI, uma ferramenta de visualização de dados
+          poderosa da Microsoft, e sua capacidade de transformar dados brutos em
+          informações significativas. Instruções detalhadas sobre como conectar
+          fontes de dados ao Power BI e criar relatórios interativos. Exemplos
+          de relatórios e dashboards criados com o Power BI, incluindo a criação
+          de um dashboard de vendas com filtros interativos e gráficos
+          dinâmicos. Página 5: Introdução ao SQL para Gerenciamento de Dados
+        </Text>
+        <img src="https://blog.unipar.br/wp-content/uploads/2021/11/post_thumbnail-801c53f03508c73882d8a372d4df2e17.jpeg" />
+        <Text>
+          Uma introdução ao Power BI, uma ferramenta de visualização de dados
+          poderosa da Microsoft, e sua capacidade de transformar dados brutos em
+          informações significativas. Instruções detalhadas sobre como conectar
+          fontes de dados ao Power BI e criar relatórios interativos. Exemplos
+          de relatórios e dashboards criados com o Power BI, incluindo a criação
+          de um dashboard de vendas com filtros interativos e gráficos
+          dinâmicos. Página 5: Introdução ao SQL para Gerenciamento de Dados
+        </Text>
+        <Text>
+          Uma introdução ao Power BI, uma ferramenta de visualização de dados
+          poderosa da Microsoft, e sua capacidade de transformar dados brutos em
+          informações significativas. Instruções detalhadas sobre como conectar
+          fontes de dados ao Power BI e criar relatórios interativos. Exemplos
+          de relatórios e dashboards criados com o Power BI, incluindo a criação
+          de um dashboard de vendas com filtros interativos e gráficos
+          dinâmicos. Página 5: Introdução ao SQL para Gerenciamento de Dados
+        </Text>
+        <Text>
+          Uma introdução ao Power BI, uma ferramenta de visualização de dados
+          poderosa da Microsoft, e sua capacidade de transformar dados brutos em
+          informações significativas. Instruções detalhadas sobre como conectar
+          fontes de dados ao Power BI e criar relatórios interativos. Exemplos
+          de relatórios e dashboards criados com o Power BI, incluindo a criação
+          de um dashboard de vendas com filtros interativos e gráficos
+          dinâmicos. Página 5: Introdução ao SQL para Gerenciamento de Dados
+        </Text>
+      </Box>
+    </Flex>
+  );
 }
