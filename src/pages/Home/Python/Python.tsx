6f6cab653a35ef3950ef879a7b4c7a79bde79fbc
FelipeGabrielSilva <felipegabriel1106@outlook.com>
2023-11-02 17:44:24 -0300

    Adicionando conteúdo #Python

--- src/pages/Home/Python/Python.tsx
+++ src/pages/Home/Python/Python.tsx
@@ -1,8 +1,61 @@
+import { Flex, Box, Text } from "@chakra-ui/react";
 
 export default function Python() {
-    return(
-        <>
-        
-        </>
-    )
+  return (
+    <Flex w="100%" h="auto" bg="#427D9D">
+      <Box display="flex" flexDir="column" w="60%" p="5em" gap="1em">
+        <img src="https://blog.unipar.br/wp-content/uploads/2021/11/post_thumbnail-801c53f03508c73882d8a372d4df2e17.jpeg" />
+        <Text fontSize="sm">Descrição imagem</Text>
+        <Text fontSize="xl" fontWeight="bold">
+          Programação em Python para Análise de Dados
+        </Text>
+        <Text>
+          Introdução ao Python como uma linguagem de programação versátil
+          amplamente usada em análise de dados devido à sua rica ecossistema de
+          bibliotecas. Como instalar o Python e as bibliotecas Pandas e
+          Matplotlib, fundamentais para análise e visualização de dados.
+          Exemplos de código Python que demonstram como realizar tarefas de
+          análise de dados, como carregar um conjunto de dados, realizar
+          filtragens e criar gráficos personalizados.
+        </Text>
+        <Text>
+          Introdução ao Python como uma linguagem de programação versátil
+          amplamente usada em análise de dados devido à sua rica ecossistema de
+          bibliotecas. Como instalar o Python e as bibliotecas Pandas e
+          Matplotlib, fundamentais para análise e visualização de dados.
+          Exemplos de código Python que demonstram como realizar tarefas de
+          análise de dados, como carregar um conjunto de dados, realizar
+          filtragens e criar gráficos personalizados.
+        </Text>
+        <img src="https://blog.unipar.br/wp-content/uploads/2021/11/post_thumbnail-801c53f03508c73882d8a372d4df2e17.jpeg" />
+        <Text>
+          Introdução ao Python como uma linguagem de programação versátil
+          amplamente usada em análise de dados devido à sua rica ecossistema de
+          bibliotecas. Como instalar o Python e as bibliotecas Pandas e
+          Matplotlib, fundamentais para análise e visualização de dados.
+          Exemplos de código Python que demonstram como realizar tarefas de
+          análise de dados, como carregar um conjunto de dados, realizar
+          filtragens e criar gráficos personalizados.
+        </Text>
+        <Text>
+          Introdução ao Python como uma linguagem de programação versátil
+          amplamente usada em análise de dados devido à sua rica ecossistema de
+          bibliotecas. Como instalar o Python e as bibliotecas Pandas e
+          Matplotlib, fundamentais para análise e visualização de dados.
+          Exemplos de código Python que demonstram como realizar tarefas de
+          análise de dados, como carregar um conjunto de dados, realizar
+          filtragens e criar gráficos personalizados.
+        </Text>
+        <Text>
+          Introdução ao Python como uma linguagem de programação versátil
+          amplamente usada em análise de dados devido à sua rica ecossistema de
+          bibliotecas. Como instalar o Python e as bibliotecas Pandas e
+          Matplotlib, fundamentais para análise e visualização de dados.
+          Exemplos de código Python que demonstram como realizar tarefas de
+          análise de dados, como carregar um conjunto de dados, realizar
+          filtragens e criar gráficos personalizados.
+        </Text>
+      </Box>
+    </Flex>
+  );
 }
