d3c06b58b9f4f8ba9756b1b03d4b4893fe8354ef
FelipeGabrielSilva <felipegabriel1106@outlook.com>
2023-11-02 17:47:01 -0300

    Adicionando conteúdo #SQL

--- src/pages/Home/IntroSQL/IntroSQL.tsx
+++ src/pages/Home/IntroSQL/IntroSQL.tsx
@@ -1,8 +1,86 @@
+import { Flex, Box, Text } from "@chakra-ui/react";
 
 export default function IntroSQL() {
-    return(
-        <>
-        
-        </>
-    )
+  return (
+    <Flex w="100%" h="auto" bg="#427D9D">
+      <Box display="flex" flexDir="column" w="60%" p="5em" gap="1em">
+        <img src="https://blog.unipar.br/wp-content/uploads/2021/11/post_thumbnail-801c53f03508c73882d8a372d4df2e17.jpeg" />
+        <Text fontSize="sm">Descrição imagem</Text>
+        <Text fontSize="xl" fontWeight="bold">
+          Introdução ao SQL para Gerenciamento de Dados
+        </Text>
+        <Text>
+          Explicação sobre o SQL (Structured Query Language) como uma linguagem
+          de consulta usada para gerenciar bancos de dados relacionais.
+          Introdução a comandos SQL fundamentais, como SELECT, FROM, WHERE e
+          JOIN, com exemplos de como eles funcionam. Exemplos de consultas SQL
+          que demonstram como recuperar e manipular dados em um banco de dados,
+          incluindo a extração de informações de uma tabela de funcionários e a
+          combinação de dados de diferentes tabelas com JOIN. Certifique-se de
+          usar exemplos concretos e, sempre que possível, inclua capturas de
+          tela ou gráficos para ilustrar os conceitos e tornar o conteúdo mais
+          envolvente. Além disso, facilite a navegação entre as páginas e torne
+          o site interativo, incentivando os visitantes a experimentar as
+          técnicas e ferramentas apresentadas.
+        </Text>
+        <Text>
+          Explicação sobre o SQL (Structured Query Language) como uma linguagem
+          de consulta usada para gerenciar bancos de dados relacionais.
+          Introdução a comandos SQL fundamentais, como SELECT, FROM, WHERE e
+          JOIN, com exemplos de como eles funcionam. Exemplos de consultas SQL
+          que demonstram como recuperar e manipular dados em um banco de dados,
+          incluindo a extração de informações de uma tabela de funcionários e a
+          combinação de dados de diferentes tabelas com JOIN. Certifique-se de
+          usar exemplos concretos e, sempre que possível, inclua capturas de
+          tela ou gráficos para ilustrar os conceitos e tornar o conteúdo mais
+          envolvente. Além disso, facilite a navegação entre as páginas e torne
+          o site interativo, incentivando os visitantes a experimentar as
+          técnicas e ferramentas apresentadas.
+        </Text>
+        <img src="https://blog.unipar.br/wp-content/uploads/2021/11/post_thumbnail-801c53f03508c73882d8a372d4df2e17.jpeg" />
+        <Text>
+          Explicação sobre o SQL (Structured Query Language) como uma linguagem
+          de consulta usada para gerenciar bancos de dados relacionais.
+          Introdução a comandos SQL fundamentais, como SELECT, FROM, WHERE e
+          JOIN, com exemplos de como eles funcionam. Exemplos de consultas SQL
+          que demonstram como recuperar e manipular dados em um banco de dados,
+          incluindo a extração de informações de uma tabela de funcionários e a
+          combinação de dados de diferentes tabelas com JOIN. Certifique-se de
+          usar exemplos concretos e, sempre que possível, inclua capturas de
+          tela ou gráficos para ilustrar os conceitos e tornar o conteúdo mais
+          envolvente. Além disso, facilite a navegação entre as páginas e torne
+          o site interativo, incentivando os visitantes a experimentar as
+          técnicas e ferramentas apresentadas.
+        </Text>
+        <Text>
+          Explicação sobre o SQL (Structured Query Language) como uma linguagem
+          de consulta usada para gerenciar bancos de dados relacionais.
+          Introdução a comandos SQL fundamentais, como SELECT, FROM, WHERE e
+          JOIN, com exemplos de como eles funcionam. Exemplos de consultas SQL
+          que demonstram como recuperar e manipular dados em um banco de dados,
+          incluindo a extração de informações de uma tabela de funcionários e a
+          combinação de dados de diferentes tabelas com JOIN. Certifique-se de
+          usar exemplos concretos e, sempre que possível, inclua capturas de
+          tela ou gráficos para ilustrar os conceitos e tornar o conteúdo mais
+          envolvente. Além disso, facilite a navegação entre as páginas e torne
+          o site interativo, incentivando os visitantes a experimentar as
+          técnicas e ferramentas apresentadas.
+        </Text>
+        <Text>
+          Explicação sobre o SQL (Structured Query Language) como uma linguagem
+          de consulta usada para gerenciar bancos de dados relacionais.
+          Introdução a comandos SQL fundamentais, como SELECT, FROM, WHERE e
+          JOIN, com exemplos de como eles funcionam. Exemplos de consultas SQL
+          que demonstram como recuperar e manipular dados em um banco de dados,
+          incluindo a extração de informações de uma tabela de funcionários e a
+          combinação de dados de diferentes tabelas com JOIN. Certifique-se de
+          usar exemplos concretos e, sempre que possível, inclua capturas de
+          tela ou gráficos para ilustrar os conceitos e tornar o conteúdo mais
+          envolvente. Além disso, facilite a navegação entre as páginas e torne
+          o site interativo, incentivando os visitantes a experimentar as
+          técnicas e ferramentas apresentadas.
+        </Text>
+      </Box>
+    </Flex>
+  );
 }
